a0146f2e34cadae936600bff90a6e260cf3fb408
Justin Ackermann <justinackermann@yahoo.com>
2022-11-29 19:44:36 -0600

    Initial Commit

---
+++ Tests/BoardingPassKitTests/BoardingPassKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BoardingPassKit
+
+final class BoardingPassKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BoardingPassKit().text, "Hello, World!")
+    }
+}
