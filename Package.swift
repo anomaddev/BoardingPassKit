59b38468a8090f8cec26b1749e4d07de3851ec5f
anomaddev <anomaddev@gmail.com>
2024-02-04 21:35:10 -0500

    working

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BoardingPassKit",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_13)
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
