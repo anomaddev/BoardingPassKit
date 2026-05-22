4249e5ff4af15627168ceeff3d8ef61a343b7da7
Justin Ackermann <anomaddev@gmail.com>
2026-05-22 00:03:58 -0700

    monorepo structure & node support added

---
+++ apps/BoardingPassKitDemo/BoardingPassKitDemoUITests/BoardingPassKitDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BoardingPassKitDemoUITestsLaunchTests.swift
+//  BoardingPassKitDemoUITests
+//
+//  Created by Justin Ackermann on 12/8/23.
+//
+
+import XCTest
+
+final class BoardingPassKitDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
